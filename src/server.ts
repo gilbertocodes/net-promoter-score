c4c4f1c9211c514b78af957dae2905026051a0db
devGilberto <gmrf1996@gmail.com>
2021-02-24 01:01:22 +0000

    Express Basics w/ routes

---
+++ src/server.ts
@@ -0,0 +1,46 @@
+import express from 'express';
+
+const app = express();
+
+
+/**
+ * GET => Encontrar
+ * POST => Guardar
+ * PUT => Alterar
+ * DELETE => Apagar
+ * PATCH => Alteração especifica
+ * 
+ */
+
+
+
+// http://localhost:9999/users
+
+app.get("/", (request, response) => {
+    return response.json(
+        {
+            message: "Hello World - NLW04"
+        }
+    );
+
+});
+
+
+// 1 param => route, API resource
+// 2 param => request, responde
+
+app.post("/", (request, response) => {
+    return response.json(
+        {
+            message: "Dados guardados com sucesso!"
+        }
+    );
+});
+
+
+
+
+app.listen(9999, () => console.log("Server is running!"));
+
+
+
