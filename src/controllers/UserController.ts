0bfd596db1b6b18a4ed0208da7e624923ff9c4e7
gilbertocodes <gmrf1996@gmail.com>
2021-02-28 01:10:44 +0000

    UserController Refactor w/ Yup Schema Validation

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,12 +1,33 @@
 import { Request, Response } from 'express';
 import { getCustomRepository } from 'typeorm';
 import { UsersRepository } from "../repositories/UsersRepository";
+import * as yup from 'yup';
  
 class UserController{
 
     async create(req: Request, res: Response){
         const { name, email } = req.body;
+
+        const schema = yup.object().shape({
+            name: yup.string().required("Name is required!!!"),
+            email: yup.string().email().required("Email is required!!!")
+        })
         
+        /* if(!(await schema.isValid(req.body))){
+           return res.status(400).json({
+               error: "Validation Failed!"
+           });
+        } */
+        try{
+           await schema.validate(req.body);
+        } catch(err){
+            return res.status(400).json({
+                error: err
+            });
+        }
+        
+        
+
         const usersRepository = getCustomRepository(UsersRepository);
 
         // SELECT * FROM users WHERE email = "email"
